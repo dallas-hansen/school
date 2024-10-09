b181836809b4eb51f5edb6d4538c583367059cfc
Dallas Hansen <dallashansen.work@email.com>
2024-10-09 13:50:22 -0400

    finished first api call

---
+++ cis_123/API/first_API_call.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+def main():
+    response = requests.get("http://api.open-notify.org/astros.json")
+    people_list = response.json()['people']
+    
+    person_name = ''
+    craft_name = ''
+    
+    for person in people_list:
+        for k,v in person.items():
+            if k == 'craft':
+                craft_name = v
+                if len(person_name) > 0 and len(craft_name) > 0:
+                    print(f'Astronaut: {person_name} is on board {craft_name}')
+            elif k == 'name':
+                person_name = v
+
+if __name__ == "__main__":
+    main()
