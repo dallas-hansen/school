8e42ebab148228f0777c37b5bccde07c48736064
Dallas Hansen <dallashansen.work@gmail.com>
2024-10-20 03:02:42 -0400

    fully working menu!!

--- cis_123/project/api_caller/api_caller.py
+++ cis_123/project/api_caller/api_caller.py
@@ -1,3 +1,4 @@
+from calendar import c
 import os
 import requests
 import json
@@ -46,8 +47,9 @@ def make_menu(options: dict, level=0) -> str:
         count += 1
         option_dict[str(count)] = key
     if level == 0:
-        #TODO: finish what happens with level 0
-        pass
+        option_dict['0'] = 'Exit'
+    else:
+        option_dict['0'] = 'Back'
     
     return make_choice(option_dict)
 
@@ -57,35 +59,41 @@ def make_choice(options: dict) -> str:
     for k,v in options.items():
         print(f'{k} - {v}')
     
-    print('0 - Exit')
     choice = input("Enter your choice: ")
     
     if choice == '0':
-        return
+        return 'exit'
     
     while choice not in options.keys():
         print('Invalid option, please try again')
         choice = input("Enter your choice: ")
 
     return options[choice]
-#TODO: figure out why this is returning None
 
 @separator()
 def list_sites(sites: list) -> None:
     print('Sites:')
     for index, site in enumerate(sites):
-        print(f'{index+1} - {site.capitalize()}')
+        print(f'{index+1} - {site.name.capitalize()}')
 
 @separator()
-def add_site(sites: list) -> None:
-    name = input('Enter site name: ').lower()
+def add_site(sites: list, name=None) -> None:
+    if name is None:
+        name = input('Enter site name: ').lower()
     name = Site(name)
     sites.append(name)
     
 def delete_site(sites: list) -> None:
     list_sites(sites)
     name = input('Enter site name: ').lower()
-    sites.remove(name)
+    for site in sites:
+        if site.name.lower() == name:
+            name = site
+            sites.remove(name)
+            print(f'Site "{name.name.capitalize()}" deleted')
+            break
+    else:
+        print('Site not found')
     
     
     
@@ -107,17 +115,26 @@ def main():
             'Delete Site': delete_site}
     
     first_time = True
+    current_choice = None
     
-    while True:
+    while current_choice != 'exit':
         if first_time:
-            choice = make_menu(menu)
-            menu[choice](sites)
+            current_choice = make_menu(menu, 0)
+            print(current_choice)
+            if current_choice != 'exit' and current_choice != 'back':
+                menu[current_choice](sites)
             first_time = False
-        break        
+        else:
+            current_choice = make_menu(menu, 0)
+            if current_choice != 'exit' and current_choice != 'back':
+                menu[current_choice](sites)
 
 if __name__ == "__main__":
     # sites = load_data('sites')
-    sites = ['Sites', 'Test', 'Test2']
+    
+    
+    sites = []
+    add_site(sites, 'Treasury')
     
     main()
     
