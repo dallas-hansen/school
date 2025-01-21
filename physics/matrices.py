9287825f5571d856cde7f4063ddd282817260567
Dallas Hansen <dallashansen.work@gmail.com>
2025-01-20 20:08:09 -0500

    some matrix stuff

--- physics/matrices.py
+++ physics/matrices.py
@@ -1,3 +1,19 @@
 import numpy as np
+import math
 
-A = np.array([[3, -4, -2], [1, -7, 4], [-7, 4, 1]])
+matrix = np.array([
+    [3, -4, -2], 
+    [-7, 4, 1], 
+    [1, -7, 4]
+])
+
+A = matrix[0]
+B = matrix[1]
+C = matrix[2]
+
+vector_sum = A + B + C
+displacement = math.sqrt(vector_sum[0] ** 2 + vector_sum[1] ** 2 + vector_sum[2] ** 2)
+
+
+print(f'Vector sum: {vector_sum}')
+print(f'Displacement: {displacement}')
